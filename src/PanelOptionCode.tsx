efd9cb10e3f15e4c5ae83df511cd46f5687d0e8a
Andrew Ballin <aballin@eh2.com>
2026-04-24 16:54:07 -0700

    feat: copilot update try 2

--- src/PanelOptionCode.tsx
+++ src/PanelOptionCode.tsx
@@ -1,9 +1,6 @@
-//Code from https://github.com/gapitio/gapit-htmlgraphics-panel
 import React from 'react';
 import { StandardEditorProps } from '@grafana/data';
-import { CodeEditor, useTheme2 } from '@grafana/ui';
-import AutoSizer from 'react-virtualized-auto-sizer';
-import { css } from '@emotion/css';
+import { CodeEditor } from '@grafana/ui';
 
 interface Props extends StandardEditorProps<string, any, any> {}
 
@@ -11,29 +8,21 @@ export const PanelOptionCode: React.FC<Props> = ({ value, item, onChange }) => {
   if (typeof value !== 'string') {
     value = JSON.stringify(value, null, 2);
   }
-  const theme = useTheme2();
   return (
-    <AutoSizer
-      disableHeight
-      className={css`
-        margin-bottom: ${theme.spacing(1)};
-      `}
-    >
-      {({ width }) => (
-        <CodeEditor
-          language={item.settings?.language}
-          showLineNumbers={item.settings?.language === 'javascript' ? true : false}
-          value={value === 'null' ? JSON.stringify(item.settings?.initValue, null, 2) : value}
-          width={width}
-          height="200px"
-          onBlur={(code) => {
-            if (item.settings?.language === 'json' && code) {
-              code = JSON.parse(code);
-            }
-            onChange(code);
-          }}
-        />
-      )}
-    </AutoSizer>
+    <div style={{ marginBottom: 8 }}>
+      <CodeEditor
+        language={item.settings?.language}
+        showLineNumbers={item.settings?.language === 'javascript'}
+        value={value === 'null' ? JSON.stringify(item.settings?.initValue, null, 2) : value}
+        width="100%"
+        height="200px"
+        onBlur={(code) => {
+          if (item.settings?.language === 'json' && code) {
+            code = JSON.parse(code);
+          }
+          onChange(code);
+        }}
+      />
+    </div>
   );
 };
